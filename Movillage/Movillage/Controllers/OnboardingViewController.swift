d0c185e87d3a010a88d9b99b92d4e50808f37149
Taerogrammer <hyung54321@naver.com>
2025-01-24 15:36:38 +0900

    feat: 코드 베이스 세팅 및 메인 스토리보드 삭제

---
+++ Movillage/Movillage/Controllers/OnboardingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OnboardingViewController.swift
+//  Movillage
+//
+//  Created by 김태형 on 1/24/25.
+//
+
+import UIKit
+
+final class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+
+}
