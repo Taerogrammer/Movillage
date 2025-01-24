766c6b56f98533ba1f2e43dd184d6830f86ec492
Taerogrammer <hyung54321@naver.com>
2025-01-25 00:40:09 +0900

    feat: 버튼 색상 에셋에 추가 및 버튼 컴포넌트 생성

---
+++ Movillage/Movillage/Components/ButtonComponent/ButtonComponent.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+final class CustomButton: UIButton {
+
+    init() {
+        super.init(frame: .zero)
+        setTitleColor(UIColor.customBlue, for: .normal)
+        titleLabel?.font = .boldSystemFont(ofSize: 14)
+        clipsToBounds = true
+        layer.borderWidth = 3
+        layer.cornerRadius = 24
+        contentEdgeInsets = UIEdgeInsets(top: 16, left: 0, bottom: 16, right: 0)
+        layer.borderColor = UIColor.customBlue.cgColor
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
