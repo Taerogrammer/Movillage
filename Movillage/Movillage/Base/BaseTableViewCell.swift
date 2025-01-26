d8af307d7769d41c32dc1fa6cba67ce17410713d
Taerogrammer <hyung54321@naver.com>
2025-01-26 17:30:00 +0900

    feat: BaseTableViewCell 생성

---
+++ Movillage/Movillage/Base/BaseTableViewCell.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+
+    func configureHierarchy() {}
+    func configureLayout() {}
+    func configureView() {}
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
