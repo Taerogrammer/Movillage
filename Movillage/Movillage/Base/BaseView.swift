926050d0ece2c2dd80758966a5653f0b2114e654
Taerogrammer <hyung54321@naver.com>
2025-01-26 10:55:52 +0900

    fix: backgroundColor 관련 설정 수정

--- Movillage/Movillage/Base/BaseView.swift
+++ Movillage/Movillage/Base/BaseView.swift
@@ -12,7 +12,10 @@ class BaseView: UIView, ViewConfiguration {
     override init(frame: CGRect) {
         super.init(frame: frame)
         [configureHierarchy(), configureLayout(), configureView()].forEach{ $0 }
-        backgroundColor = .customBlack
+
+        // 해당 init이 마지막에 호출되어 backgroundColor를 수정해도 덮어쓰여짐
+        // 만약 backgroundColor가 없다면 black으로 지정
+        if backgroundColor == nil { backgroundColor = .customBlack }
     }
 
     @available(*, unavailable)
---
+++ Movillage/Movillage/Views/Profile/ProfileCardView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileCardView.swift
+//  Movillage
+//
+//  Created by 김태형 on 1/26/25.
+//
+
+import UIKit
+
+class ProfileCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
