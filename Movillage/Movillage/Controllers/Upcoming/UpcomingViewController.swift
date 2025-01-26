dfccbe79fc79ec58b2bd98cbd6b6d243a1947f71
Taerogrammer <hyung54321@naver.com>
2025-01-26 10:11:03 +0900

    feat: upcoming 디자인

--- Movillage/Movillage/Controllers/Upcoming/UpcomingViewController.swift
+++ Movillage/Movillage/Controllers/Upcoming/UpcomingViewController.swift
@@ -1,10 +1,20 @@
 import UIKit
 
 final class UpcomingViewController: UIViewController {
+    private let upcomingView = UpcomingView()
 
+    override func loadView() {
+        view = upcomingView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("upcoming")
+        configureNavigation()
     }
+}
 
+// MARK: configure navigation
+extension UpcomingViewController: NavigationConfiguration {
+    func configureNavigation() {
+        navigationItem.title = "UPCOMING"
+    }
 }
--- Movillage/Movillage/Views/Upcoming/UpcomingView.swift
+++ Movillage/Movillage/Views/Upcoming/UpcomingView.swift
@@ -1,5 +1,18 @@
 import UIKit
+import SnapKit
 
 final class UpcomingView: BaseView {
+    private let upcomingLabel = ContentRegularLabel()
 
+    override func configureHierarchy() {
+        addSubview(upcomingLabel)
+    }
+    override func configureLayout() {
+        upcomingLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    override func configureView() {
+        upcomingLabel.text = "추후 업데이트 예정입니다"
+    }
 }
