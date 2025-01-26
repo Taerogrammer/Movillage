b53e9047cfd36005fede2d512e3aa106301960ca
Taerogrammer <hyung54321@naver.com>
2025-01-26 11:21:41 +0900

    feat: 프로필 카드뷰 디자인

--- Movillage/Movillage/Views/Profile/ProfileCardView.swift
+++ Movillage/Movillage/Views/Profile/ProfileCardView.swift
@@ -1,20 +1,55 @@
-//
-//  ProfileCardView.swift
-//  Movillage
-//
-//  Created by 김태형 on 1/26/25.
-//
-
 import UIKit
+import SnapKit
 
-class ProfileCardView: UIView {
+final class ProfileCardView: BaseView {
+    private let profileImage = ProfileImage(frame: CGRect())
+    private let nicknameLabel = HeaderLabel()
+    private let registerLabel = DescriptionLabel()
+    private let likeCountButton = UIButton()
+    private let chevronImage = UIImageView()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func configureHierarchy() {
+        [profileImage, nicknameLabel, registerLabel, likeCountButton, chevronImage].forEach { addSubview($0) }
     }
-    */
-
+    override func configureLayout() {
+        profileImage.snp.makeConstraints {
+            $0.leading.top.equalTo(self.safeAreaLayoutGuide).inset(16)
+            $0.width.height.equalTo(60)
+        }
+        nicknameLabel.snp.makeConstraints {
+            $0.leading.equalTo(profileImage.snp.trailing).offset(12)
+            $0.top.equalTo(profileImage).inset(12)
+        }
+        registerLabel.snp.makeConstraints {
+            $0.leading.equalTo(nicknameLabel)
+            $0.top.equalTo(nicknameLabel.snp.bottom).offset(12)
+        }
+        chevronImage.snp.makeConstraints {
+            $0.trailing.equalTo(self.safeAreaLayoutGuide).inset(16)
+            $0.centerY.equalTo(profileImage)
+        }
+        likeCountButton.snp.makeConstraints {
+            $0.leading.equalTo(profileImage)
+            $0.top.equalTo(profileImage.snp.bottom).offset(16)
+            $0.trailing.bottom.equalTo(self.safeAreaLayoutGuide).inset(16)
+            $0.height.lessThanOrEqualTo(36)
+        }
+    }
+    override func configureView() {
+        clipsToBounds = true
+        backgroundColor = UIColor.customGray
+        layer.cornerRadius = 12
+        profileImage.image = UIImage(systemName: "person")
+        nicknameLabel.text = "테스트용 이름입니다."
+        registerLabel.text = "25.01.24 가입"
+        chevronImage.image = UIImage(systemName: "chevron.right")
+        chevronImage.tintColor = UIColor.customWhiteGray
+        likeCountButton.setTitle("0개의 무비박스 보관중", for: .normal)
+        likeCountButton.clipsToBounds = true
+        likeCountButton.backgroundColor = UIColor.customBlue
+        likeCountButton.layer.cornerRadius = 8
+        likeCountButton.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        likeCountButton.titleLabel?.textColor = UIColor.customWhite
+        likeCountButton.contentEdgeInsets = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
 }
