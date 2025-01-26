e4b45de3c57aceada2df9fc67c733f83a67e5104
Taerogrammer <hyung54321@naver.com>
2025-01-26 15:16:09 +0900

    feat: button 추가

--- Movillage/Movillage/Controllers/Profile/ProfileEditViewController.swift
+++ Movillage/Movillage/Controllers/Profile/ProfileEditViewController.swift
@@ -10,5 +10,22 @@ final class ProfileEditViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureButton()
+    }
+}
+
+// MARK: configure button
+extension ProfileEditViewController: ButtonConfiguration {
+    func configureButton() {
+        profileEditView.closeButton.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
+        profileEditView.saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
+    }
+}
+
+// MARK: @objc
+extension ProfileEditViewController {
+    @objc private func closeButtonTapped() { dismiss(animated: true) }
+    @objc private func saveButtonTapped() {
+        print(#function)
     }
 }
