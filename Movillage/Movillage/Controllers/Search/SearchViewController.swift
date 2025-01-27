5991ffdb63d38c07b7cbba32ae667274569bbc4f
Taerogrammer <hyung54321@naver.com>
2025-01-27 22:46:15 +0900

    feat: 영화 검색 데이터 불러오기 구현

--- Movillage/Movillage/Controllers/Search/SearchViewController.swift
+++ Movillage/Movillage/Controllers/Search/SearchViewController.swift
@@ -3,6 +3,8 @@ import UIKit
 final class SearchViewController: UIViewController {
 
     private let searchView = SearchView()
+    private var searchDTO = SearchDTO(query: "", page: 1)
+    private var page = 1
 
     override func loadView() {
         view = searchView
@@ -10,7 +12,7 @@ final class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        [configureNavigation()].forEach { $0 }
+        [configureNavigation(), configureDelegate()].forEach { $0 }
     }
 }
 
@@ -18,5 +20,29 @@ final class SearchViewController: UIViewController {
 extension SearchViewController: NavigationConfiguration {
     func configureNavigation() {
         navigationItem.title = "영화 검색"
+    }
+}
+
+extension SearchViewController: DelegateConfiguration {
+    func configureDelegate() {
+        searchView.searchBar.delegate = self
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print(#function, searchBar.text!)
+        searchDTO.query = searchBar.text!
+        searchDTO.page = self.page
+
+        NetworkManager.shared.fetchItem(api: searchDTO.toRequest(),
+                                        type: SearchResponse.self) { result in
+            switch result {
+            case .success(let success):
+                print("성공 -> ", success)
+            case .failure(let failure):
+                print("실패 ", failure)
+            }
+        }
     }
 }
