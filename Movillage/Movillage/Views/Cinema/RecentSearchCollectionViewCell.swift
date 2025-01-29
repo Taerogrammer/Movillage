2a671918df640d260e765a58ba24d8838260151a
Taerogrammer <hyung54321@naver.com>
2025-01-30 00:38:01 +0900

    refactor: 최근검색어 버튼 구현 방식 변경

--- Movillage/Movillage/Views/Cinema/RecentSearchCollectionViewCell.swift
+++ Movillage/Movillage/Views/Cinema/RecentSearchCollectionViewCell.swift
@@ -3,24 +3,35 @@ import SnapKit
 
 final class RecentSearchCollectionViewCell: BaseCollectionViewCell {
     static let id = "RecentSearchCollectionViewCell"
-    let researchLabel = RecentSearchButton()
+    let researchLabel = UILabel().setFont(.description)
+    let xButton = UIButton()
 
     override func configureHierarchy() {
-        contentView.addSubview(researchLabel)
+        [researchLabel, xButton].forEach { contentView.addSubview($0) }
     }
     override func configureLayout() {
         researchLabel.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+            $0.leading.equalToSuperview().offset(16)
+            $0.verticalEdges.equalToSuperview().inset(8)
+        }
+        xButton.snp.makeConstraints {
+            $0.leading.equalTo(researchLabel.snp.trailing).offset(8)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.centerY.equalTo(researchLabel)
         }
     }
     override func configureView() {
-        researchLabel.isUserInteractionEnabled = false
+        researchLabel.textColor = UIColor.customBlack
+        clipsToBounds = true
+        layer.cornerRadius = 20
+        xButton.isUserInteractionEnabled = false
+        xButton.setImage(UIImage(systemName: "xmark"), for: .normal)
+        xButton.tintColor = UIColor.customBlack
+        backgroundColor = UIColor.customWhite
     }
 }
 
 // MARK: configure cell
 extension RecentSearchCollectionViewCell {
-    func configureCell(text: String) {
-        researchLabel.setTitle(text + " ", for: .normal)
-    }
+    func configureCell(text: String) { researchLabel.text = text }
 }
