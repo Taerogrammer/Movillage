fb35ce05dd352453ead9ddc068a24c106dcfeb37
Taerogrammer <hyung54321@naver.com>
2025-01-30 22:40:17 +0900

    fix: PageControl 제거

--- Movillage/Movillage/Views/CinemaDetail/BackdropCollectionViewCell.swift
+++ Movillage/Movillage/Views/CinemaDetail/BackdropCollectionViewCell.swift
@@ -5,25 +5,18 @@ import SnapKit
 final class BackdropCollectionViewCell: BaseCollectionViewCell {
     static let id = "BackdropCollectionViewCell"
     let backdropImage = UIImageView()
-    let pageControl = UIPageControl()
 
     override func configureHierarchy() {
-        [backdropImage, pageControl].forEach { contentView.addSubview($0) }
+        [backdropImage].forEach { contentView.addSubview($0) }
     }
     override func configureLayout() {
         backdropImage.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
-        pageControl.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.bottom.equalToSuperview().inset(12)
-        }
     }
     override func configureView() {
         backdropImage.backgroundColor = UIColor.customBlack
         backdropImage.contentMode = .scaleAspectFill
-        pageControl.currentPage = 0
-        pageControl.numberOfPages = 5
     }
 }
 
